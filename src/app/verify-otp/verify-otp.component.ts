dc5af5f1aedd739aab4270cf599dcead38fc8a29
mohamedeleebs <eleebsmohamed@gmail.com>
2025-04-23 06:16:16 +0200

    01 translating login and regestair  and other

--- src/app/verify-otp/verify-otp.component.ts
+++ src/app/verify-otp/verify-otp.component.ts
@@ -4,12 +4,13 @@ import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-verify-otp',
   templateUrl: './verify-otp.component.html',
   styleUrls: ['./verify-otp.component.css'],
-  imports: [CommonModule, RouterLink, FormsModule],
+  imports: [CommonModule, RouterLink, FormsModule,TranslateModule],
 })
 export class VerifyOtpComponent {
   email: string = '';
